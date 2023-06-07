f91ddd18c3bfc1de9d9974c6a120a2ffeca8339d
Ryan Punwasi <ryan.punwasi@gmail.com>
2023-06-07 19:17:49 -0400

    Refactor to use react router config

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
 
-const Routes = () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/users" component={UsersList}></Route>
-    </div>
-  );
-};
-
-export default Routes;
+export default [
+  {
+    path: "/",
+    component: Home,
+    exact: true,
+  },
+  {
+    path: "/users",
+    component: UsersList,
+    exact: true,
+  },
+];
