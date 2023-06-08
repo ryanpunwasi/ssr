cf128fb6bb32f7f80bf71631d926004f4e6941b0
Ryan Punwasi <ryan.punwasi@gmail.com>
2023-06-08 18:03:44 -0400

    Set up proxy

--- build/bundle.js
+++ build/bundle.js
@@ -188,6 +188,10 @@ var _express = __webpack_require__(8);
 
 var _express2 = _interopRequireDefault(_express);
 
+var _expressHttpProxy = __webpack_require__(22);
+
+var _expressHttpProxy2 = _interopRequireDefault(_expressHttpProxy);
+
 var _reactRouterConfig = __webpack_require__(1);
 
 var _Routes = __webpack_require__(2);
@@ -205,6 +209,12 @@ var _createStore2 = _interopRequireDefault(_createStore);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+app.use("/api", (0, _expressHttpProxy2.default)("http://react-ssr-api.herokuapp.com", {
+  proxyReqOptDecorator: function proxyReqOptDecorator(opts) {
+    opts.header["x-forwarded-host"] = "localhost:3000";
+    return opts;
+  }
+}));
 app.use(_express2.default.static("public"));
 app.get("*", function (req, res) {
   var store = (0, _createStore2.default)();
@@ -521,6 +531,12 @@ exports.default = {
 /***/ (function(module, exports) {
 
 module.exports = require("serialize-javascript");
+
+/***/ }),
+/* 22 */
+/***/ (function(module, exports) {
+
+module.exports = require("express-http-proxy");
 
 /***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "babel-polyfill";
 import express from "express";
+import proxy from "express-http-proxy";
 import { matchRoutes } from "react-router-config";
 import Routes from "./client/Routes.js";
 import renderer from "./helpers/renderer.js";
@@ -7,6 +8,15 @@ import renderer from "./helpers/renderer.js";
 import createStore from "./helpers/createStore.js";
 
 const app = express();
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.header["x-forwarded-host"] = "localhost:3000";
+      return opts;
+    },
+  })
+);
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore();
